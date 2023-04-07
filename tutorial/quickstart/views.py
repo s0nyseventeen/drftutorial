7ddbc4bef7b80e66bb553ede8493ec4743d815fe
503282404 <stanislav.kryshchuk@ge.com>
2023-04-07 16:11:14 +0300

    Added project

---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from pprint import pprint as pp
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows users to be viewed or edited"""
+    print('UserViewSet')
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    print('GroupViewSet')
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
